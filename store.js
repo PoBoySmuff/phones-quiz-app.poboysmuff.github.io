5ccdb6f6f1026fc57d8c8f5f79302f77230c515e
Thomas Poley <thomasjpoley@gmail.com>
2020-02-23 17:32:46 -0500

    store.txt converted to store.js

---
+++ store.js
@@ -0,0 +1,63 @@
+const STORE = {
+    questions: [//1
+        {
+            question: "What is the first phone to innovate the selfie camera with a pop-up camera, elimitating the camera notch from the front screen?",
+            options: [
+                "OnePlus 7 Pro",
+                "Samsung Galaxy S10",
+                "Apple iPhone 11",
+                "Motorola E7"
+            ],
+            answer: "OnePlus 7 Pro"
+        },
+        
+        {//2
+            question: "How many cameras are on the iPhone XS?",
+            options: [
+                "1",
+                "2",
+                "3",
+                "4"
+            ],
+            answer: "2"
+        },
+
+        {//3
+            question: "What is the first phone to feature 5 back-facing cameras?",
+            options: [
+                "Google Pixel 4",
+                "LG Tribute Empire",
+                "Nokia 9 PureView",
+                "Samsung Note 10+"
+            ],
+            answer: "Nokia 9 PureView"
+        },
+
+        {//4
+            question: `What phone was the first to implement night mode, allowing 
+            for clear immages in the dark without the use of flash?`,
+            options: [
+                "LG G8 ThinQ",
+                "Google Pixel 3",
+                "iPhone 11",
+                "iPhone 7"
+            ],
+            answer: "Google Pixel 3"
+        },
+
+        {//5
+            question: "Which of these is not a feature of the iPhone 11?",
+            options: [
+                "Night mode",
+                "Face ID",
+                "Wireless Powershare",
+                "Dual back-facing camera"
+            ],
+            answer: "Wireless Powershare"
+        },
+
+
+    ],
+    currentQuestion: 0,
+    score: 0
+};
